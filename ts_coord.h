5d6bd434cc1a4c7e1912733f4a2120ffe50fafba
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2016-12-09 23:39:58 +0200

    dont need this code anymore

--- ts_coord.h
+++ ts_coord.h
@@ -5,8 +5,4 @@
 
 typedef struct { uint8_t y, x; } ts_Coord;
 ts_Coord ts_Coord_new(uint8_t y, uint8_t x);
-bool ts_Coord_downCollision(ts_Coord *top, ts_Coord *bottom);
-bool ts_Coords_downCollision(ts_Coord *top, uint8_t topSize, ts_Coord *bottom, uint8_t bottomSize);
-bool ts_Coord_leftCollision(ts_Coord *right, ts_Coord *left);
-bool ts_Coords_leftCollision(ts_Coord *right, uint8_t rightSize, ts_Coord *left, uint8_t leftSize);
 #endif
